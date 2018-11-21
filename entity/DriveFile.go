d555a97680d6fc702fb197c3b34a895b1358fb22
phudnguyen <phudnguyen@kms-technology.com>
2018-11-21 10:37:59 +0700

    added drive files to track model

--- entity/DriveFile.go
+++ entity/DriveFile.go
@@ -3,11 +3,11 @@ package entity
 import "github.com/globalsign/mgo/bson"
 
 type DriveFile struct {
-	Id bson.ObjectId `json:"id" bson:"_id"`
-	TrackId string `json:"trackId" bson:"trackId"`
-	Quality string `json:"quality" bson:"quality"`
-	Name string `json:"name" bson:"name"`
-	Size int64 `json:"size" bson:"size"`
-	DriveFileId string `json:"driveId" bson:"driveId"`
-	DriveAccount string `json:"driveAccount" bson:"driveAccount"`
+	Id           bson.ObjectId `json:"id" bson:"_id"`
+	TrackId      bson.ObjectId `json:"trackId" bson:"trackId"`
+	Quality      string        `json:"quality" bson:"quality"`
+	Name         string        `json:"name" bson:"name"`
+	Size         int64         `json:"size" bson:"size"`
+	DriveFileId  string        `json:"driveId" bson:"driveId"`
+	DriveAccount bson.ObjectId `json:"driveAccount" bson:"driveAccount"`
 }
